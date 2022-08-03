1b0d95bb7d37e0cad9f0e9503d514c49ae8f4994
doaa mostafa <50427984+doaamostafa67988@users.noreply.github.com>
2022-08-03 18:36:18 +0200

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,8 +1,15 @@
+from random import randint 
 cp=randint(0,100)
 counter=0
 while True:
+    
+    try:
+        user=int(input("enter number between 1 and 100 \n"))
+    except ValueError:
+        print("This is not a valid age.")
+        continue
+ 
     counter+=1
-    user=int(input("enter number between 1 and 100 \n"))
     if cp==user:
         print('counter',counter)
         print('yes, you guess right')
@@ -10,5 +17,5 @@ while True:
     elif cp < user:
         print('your guess is high')
         
-    elif cp > user:
+    else:
         print(' your guess is low')
